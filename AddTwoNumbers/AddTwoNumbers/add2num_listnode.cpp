6851102399bc3921a154e3412f3e24d4016424fe
EsmeZ <xiaoxuanzhang9@gmail.com>
2018-01-19 22:21:46 -0500

    Solve problem using pushNode function twice

--- AddTwoNumbers/AddTwoNumbers/add2num_listnode.cpp
+++ AddTwoNumbers/AddTwoNumbers/add2num_listnode.cpp
@@ -10,10 +10,87 @@ struct ListNode{
     ListNode(int x) : val(x), next(NULL) {}
 };
 
+void pushNode(ListNode** ListHead, int val){
+    ListNode* newNode = new ListNode(val);
+    // link the old list to the new node
+    newNode->next = (*ListHead);
+    // move the head to point to new node
+    (*ListHead) = newNode;
+}
+
+void printList(ListNode* list){
+    while (list->next){
+        cout << list->val << " -> ";
+        list = list->next;
+    }
+    cout << list->val << endl;
+}
+
 // https://www.geeksforgeeks.org/add-two-numbers-represented-by-linked-lists/
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* l3 = NULL;
 
+        int sum, res;
+        while (l1 || l2){
+            // if one list is shorter than the other, then fill in 0
+            if (!l1) l1 = new ListNode(0);
+            if (!l2) l2 = new ListNode(0);
+
+            sum = l1->val + l2->val;
+            res = sum % 10;
+            if (sum >= 10){
+                if (l1->next) ++l1->next->val;
+                else l1->next = new ListNode(1);
+            }
+            
+            l1 = l1->next;
+            l2 = l2->next;
+
+            if (!l3) l3 = new ListNode(res);
+            else pushNode(&l3, res);
+        }
+
+        ListNode* l4 = NULL;
+        while (l3){
+            pushNode(&l4, l3->val);
+            l3 = l3->next;
+        }
+
+        return l4;
     }
 };
+
+
+int main() {
+
+    ListNode* list1 = NULL;
+    ListNode* list2 = NULL;
+
+    // fill in list1
+    pushNode(&list1, 5);
+    pushNode(&list1, 2);
+    pushNode(&list1, 4);
+    pushNode(&list1, 9);
+
+    cout << "List 1 is ";
+    printList(list1);
+
+    // fill in list2
+    pushNode(&list2, 8);
+    pushNode(&list2, 2);
+    pushNode(&list2, 6);
+    pushNode(&list2, 7);
+
+    cout << "List 2 is ";
+    printList(list2);
+
+    Solution sln;
+    ListNode* list_sum = sln.addTwoNumbers(list1, list2);
+
+    cout << "The sum is ";
+    printList(list_sum);
+
+    return 0;
+}
