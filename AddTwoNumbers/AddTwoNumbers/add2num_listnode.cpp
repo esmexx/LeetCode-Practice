3e50e15ac6d7a7544a0cdac3e4d7d06a83b4bb91
EsmeZ <xiaoxuanzhang9@gmail.com>
2018-01-19 21:18:55 -0500

    Reformat question into cpp code

--- AddTwoNumbers/AddTwoNumbers/add2num_listnode.cpp
+++ AddTwoNumbers/AddTwoNumbers/add2num_listnode.cpp
@@ -1,11 +1,16 @@
-/**
-* Definition for singly-linked list.
-* struct ListNode {
-*     int val;
-*     ListNode *next;
-*     ListNode(int x) : val(x), next(NULL) {}
-* };
-*/
+
+#include <iostream>
+using namespace std;
+
+// https://leetcode.com/problems/add-two-numbers/description/
+// Definition for singly-linked list
+struct ListNode{
+    int val;
+	ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+// https://www.geeksforgeeks.org/add-two-numbers-represented-by-linked-lists/
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
