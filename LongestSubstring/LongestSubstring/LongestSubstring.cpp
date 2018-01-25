ebcdc95e74625e50ae0645d50d0de17c029b7dd3
EsmeZ <xiaoxuanzhang9@gmail.com>
2018-01-24 21:42:09 -0500

    Problem statement of the Longest String question

---
+++ LongestSubstring/LongestSubstring/LongestSubstring.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+
+    }
+};
