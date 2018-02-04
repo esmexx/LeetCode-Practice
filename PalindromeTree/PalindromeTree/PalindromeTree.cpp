40dc06f4d4d8a3f7e31240b9415c3a68551fd695
EsmeZ <xiaoxuanzhang9@gmail.com>
2018-02-04 13:33:51 -0500

    Reformat the initialization of Palindrome Tree

--- PalindromeTree/PalindromeTree/PalindromeTree.cpp
+++ PalindromeTree/PalindromeTree/PalindromeTree.cpp
@@ -1,33 +1,46 @@
 #include <iostream>
-#define MAXLEN 1000
+#define MAXN 1000
 using namespace std;
 
 // EERTREE: An Ecient Data Structure for Processing Palindromes in Strings
 // arXiv:1506.04862
+// http://adilet.org/blog/25-09-14/
 
-class TreeNode {
-public:
-    int NodeLen;
-    int SuffixLink; // points to the longest proper suffix-palindrome of the node
+struct node{
+    int len; // length of the palindrome string stored in the node
+    int next[26]; // directed edge 
+    int sufflink; // suffix link pointing to the largest suffix-palindrome
 };
 
-class EERTree {
 
-    TreeNode tree[MAXLEN];
+string s; // store the input string
+node tree[MAXN]; // the eertree of a string S of length n is of size O(n)
 
-public:
-    void InitTree(){
-        // node -1
-        tree[0].NodeLen = -1;
-        tree[0].SuffixLink = 0; // pointing to self
-        // node 0
-        tree[1].NodeLen = 0;
-        tree[1].SuffixLink = 0; // pointing to node -1        
-    }
+void addLetter(int pos){
+    int let = s[pos] - 'a'; // convert char to number
 
-};
+
+
+}
+
+void initTree(){
+    // node 1: root with len -1, node 2: root with len 0
+    tree[1].len = -1;
+    tree[1].sufflink = 1;
+    tree[2].len = 0;
+    tree[2].sufflink = 1;
+}
 
 int main(){
 
+    s = "eertree";
+
+    initTree();
+
+    for (int i = 0; i < s.length(); ++i){
+        addLetter(i);
+    }
+
     return 0;
 }
+
