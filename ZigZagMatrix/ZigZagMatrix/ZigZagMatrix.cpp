5d9b5d17755b2238eea456ccbe244f68b1f32581
EsmeZ <xiaoxuanzhang9@gmail.com>
2018-02-07 10:44:52 -0500

    Problem statement of ZigZag conversion

---
+++ ZigZagMatrix/ZigZagMatrix/ZigZagMatrix.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+// Write the code that will take a string and make this conversion given a number of rows:
+// string convert(string text, int nRows);
+// convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
+
+// https://leetcode.com/problems/zigzag-conversion/description/
+
+class Solution {
+public:
+    string convert(string s, int numRows) {
+
+    }
+};
