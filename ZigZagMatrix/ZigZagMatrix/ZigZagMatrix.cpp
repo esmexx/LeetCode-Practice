c14b067169e3c6204d7ee4873ec770b78caac538
EsmeZ <xiaoxuanzhang9@gmail.com>
2018-02-12 23:03:44 -0500

    Solve ZigZag string conversion problem using string pushbacks

--- ZigZagMatrix/ZigZagMatrix/ZigZagMatrix.cpp
+++ ZigZagMatrix/ZigZagMatrix/ZigZagMatrix.cpp
@@ -7,10 +7,48 @@ using namespace std;
 // convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
 
 // https://leetcode.com/problems/zigzag-conversion/description/
-
+// https://www.geeksforgeeks.org/print-concatenation-of-zig-zag-string-form-in-n-rows/
 class Solution {
 public:
     string convert(string s, int numRows) {
 
+        int len = s.length();
+
+        if (len <= numRows || numRows == 1){
+            return s;
+        }
+        else {
+            string* sz = new string[numRows];
+            bool down; // whether moving down the zig-zag
+            int row = 0;
+
+            for (int i = 0; i < len; ++i){
+                sz[row].push_back(s[i]);
+
+                if (row == 0) down = true;
+                else if (row == numRows - 1) down = false;
+
+                (down) ? ++row : --row;
+            }
+
+            // prevent memory leak
+            string szstr = sz[0];
+            for (int i = 1; i < numRows; ++i)
+                szstr += sz[i];
+
+            delete[] sz;
+
+            return szstr;
+        }
+
     }
 };
+
+int main(){
+
+    Solution sln;
+    printf("%s\n", sln.convert("abcde", 2).c_str());
+
+    return 0;
+
+}
